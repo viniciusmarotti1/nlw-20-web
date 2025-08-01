94fbae1da57131aefad09b1e6359200349046c65
Vinicius Marotti <vinicius.marotti@hotmail.com>
2025-07-31 22:21:37 -0300

    finishing app

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CreateRoom } from "./pages/create-room";
+import { RecordRoomAudio } from "./pages/record-room-audio";
 import { Room } from "./pages/room";
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ export function App() {
 				<Routes>
 					<Route index element={<CreateRoom />} />
 					<Route path="/room/:roomId" element={<Room />} />
+					<Route path="/room/:roomId/audio" element={<RecordRoomAudio />} />
 				</Routes>
 			</BrowserRouter>
 		</QueryClientProvider>
